95a1e9e5f41160b3a94d97b27e750e70f83c3dbf
Ma1h01 <yihaomai@gmail.com>
2024-02-24 14:27:57 -0500

    use data frame instead of dict to store order and label info

--- SwiftLabelMatcher.py
+++ SwiftLabelMatcher.py
@@ -2,68 +2,65 @@ import pandas as pd
 import fitz
 import subprocess
 
+def set_up_table(excel_file_path, pdf_file_path):
+    try:
+        shipping_labels_pdf = fitz.open(pdf_file_path)
+        order_details_excel = pd.read_excel(excel_file_path)
+    except Exception as e:
+        print(e)
+        quit()
 
+    # copy the data frame, containing Order Number and ID columns only        
+    table = order_details_excel[['Order Number', 'ID']]
+    # use the Order Number as the index
+    table.set_index('Order Number', inplace=True)    
+
+    for page_num in range(shipping_labels_pdf.page_count):
+        page = shipping_labels_pdf[page_num]
+        # extract all the text from the page into a list of strings
+        order_number = page.get_text().splitlines()
+        # since the order number can be in the second to last or last of the list, we need to check
+        # order number contains NO alphabets
+        order_number = order_number[-1] if (any(char.isalpha() for char in order_number[-2])) else order_number[-2]
+        # add the page number and printed status to the table
+        table.loc[order_number, ['Page Number', 'Printed']] = page_num + 1, False
+
+    shipping_labels_pdf.close()
+    return table
+
+## Start of the program
+excel_file_path = f'./orders/{input("Enter Excel document name: ")}.xlsx'
 pdf_file_path = f'./orders/{input("Enter PDF document name: ")}.pdf'
-try:
-    pdf_doc = fitz.open(pdf_file_path)
-except Exception as e:
-    print(e)
-    print("Unable to find the PDF document")
-    quit()
 
-product_ids = {}
-for page_num in range(pdf_doc.page_count):
-    page = pdf_doc[page_num]  #  SHG162
-    id = page.get_text().splitlines()[-2]  # 合并PDF
-    if id not in product_ids:
-        product_ids[id] = [[page_num + 1, False]] #[page, printed?]
-    else:
-        product_ids[id].append([page_num + 1, False])
-print(product_ids)
+table = set_up_table(excel_file_path, pdf_file_path)
 
 while True:
     target_id = input("Product Id: ")
     if target_id.lower() == 'q':
         print("Exiting...")
         break
-    # if target_text.lower() == 'q':
-    #     print("Exiting...")
-    #     break
-    # page_has_found = False
-    # for page_num in range(pdf_doc.page_count):
-    #     page = pdf_doc[page_num]  #  SHG162
-    #     text = page.get_text('text')  # 合并PDF
 
-    #     if target_text in text:
-    #         print(f'text type: {type(text)}')
-    #         print(f'text: {text}')
-    #         print(f'to array: {text.splitlines()}')
-    #         print(f'id: {text.splitlines()[-2]}')
-    #         print(f"Printing page {page_num + 1}")
-    #         page_has_found = True
-    #         # subprocess.run(["lp", "-d", "Zebra_Technologies_ZTC_ZP_450_200dpi", "-o", f"page-ranges={page_num+1}", pdf_file_path])
-    #         break
-    if target_id not in product_ids:
-        print("Label not found")
-    elif False not in [x[1] for x in product_ids[target_id]]:
-        response = input("All labels for this product have been printed, enter 'y' to print again, 'n' to exit: ")
-        if response.lower() == 'y':
-            for x in product_ids[target_id]:
-                x[1] = False
-        else:
-            print("Exiting...")
-            break
+    # check if there is an order number contains the specified product ID
+    filtered_table = table[table['ID'] == target_id]
+    if len(filtered_table) == 0:
+        print(f"No orders found for product {target_id}")
+        continue
     else:
-        count = 0
-        for x in product_ids[target_id]:
-            if not x[1]:
-                print(f"Printing page {x[0]}")
-                x[1] = True
-                count += 1
-                print(f'{target_id}: {count} of {len(product_ids[target_id])}')
-                # subprocess.run(["lp", "-d", "Zebra_Technologies_ZTC_ZP_450_200dpi", "-o", f"page-ranges={x[0]}", pdf_file_path])
+        # check if all orders corresponding to the product ID have been printed
+        filtered_table_unprinted = filtered_table[filtered_table['Printed'] == False]
+        if len(filtered_table_unprinted) == 0:
+            print(f"All labels for product {target_id} have been printed")
+            response = input("Enter 'y' to print again, 'n' to continue: ")
+            if response.lower() == 'y':
+                # reset all the printed status to False
+                for index,row in filtered_table.iterrows():
+                    table.loc[index, 'Printed'] = False
+        else:
+            for index,row in filtered_table_unprinted.iterrows():
+                print(f"{target_id}: {len(filtered_table) - len(filtered_table_unprinted) + 1} of {len(filtered_table)}")
+                print(f"Printing page {int(row['Page Number'])} for order {index} and product {target_id}")
+                subprocess.run(["lp", "-d", "Zebra_Technologies_ZTC_ZP_450_200dpi", "-o", f"page-ranges={int(row['Page Number'])}", "-o", "print-quality=5", pdf_file_path])
+                table.loc[index, 'Printed'] = True
                 break
-            count += 1
 
-pdf_doc.close()
 
